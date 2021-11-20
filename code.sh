34a11024f4ededfdc32cfa50a1b5a7311ca90f22
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2021-11-20 15:52:26 +0100

    Update and rename code to code.sh

--- code.sh
+++ code.sh
@@ -1,4 +1,10 @@
-mkdir Appimages && cd Appimages
+#! /bin/bash
+echo "Enter path name of dir":
+read filename
+mkdir -p $filename
+cd  $filename
+
+
 find ~/ -type f -name "*.AppImage" |  xargs cp -t.
 for i in *.AppImage; do mkdir -p "${i}".home; done;
 
