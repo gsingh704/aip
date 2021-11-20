e4fcffde606bbebdf6a5bce6d4a6ebe7821352cc
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2021-11-20 18:14:41 +0100

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,8 +1,21 @@
-echo "Enter path  of directory where you want to store the appimages":
-read filename
-mkdir -p $filename
-cd  $filename
-find ~/ -type f -name "*.AppImage" |  xargs cp -t.
-for i in *.AppImage; do mkdir -p "${i}".home; done;
+#!/bin/bash
 
+echo -n "Enter path of directory where you want to store the appimages":
+read dirname
+mkdir -p $dirname
+cd $dirname
+
+# you really want IFS set to a newline to prevent unwanted black magic from happening, unless you're 101% sure there won't be filenames with whitespace
+IFS='
+'
+
+filelist=$(find ~/ -type f -name '*.AppImage')
+for item in $filelist; do
+  # skip things you don't want
+  [[ $item =~ Trash ]] && continue
+
+  filename=${item##*/}
+  mkdir -p "${filename}.home"
+  cp "$item" .
+done;
 
