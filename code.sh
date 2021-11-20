84ac062ef5629c84d148840fe6b206f00206dbbb
gsingh704 <85999340+gsingh704@users.noreply.github.com>
2021-11-20 15:57:43 +0100

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,10 +1,7 @@
-
 echo "Enter path  of directory where you want to store the appimages":
 read filename
 mkdir -p $filename
 cd  $filename
-
-
 find ~/ -type f -name "*.AppImage" |  xargs cp -t.
 for i in *.AppImage; do mkdir -p "${i}".home; done;
 
